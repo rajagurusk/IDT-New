2f2713c2d33bb514dda2628884331520b633f4e8
rajagurusk <rajagurusivakumar1@gmail.com>
2025-08-07 11:55:02 +0530

    login editing

---
+++ IDT New/Controllers/AccountController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class AccountController : Controller
+{
+    [HttpGet]
+    public IActionResult Login()
+    {
+        return View();
+    }
+    // (Optional: Add a [HttpPost] Login method later for actual authentication)
+}
