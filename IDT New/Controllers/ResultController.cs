66038f3981569403cf6aadafee3bf2ed29b27467
rajagurusk <rajagurusivakumar1@gmail.com>
2025-08-13 16:08:55 +0530

    certificate and result updated

---
+++ IDT New/Controllers/ResultController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class ResultController : Controller
+{
+    // GET: /Result
+    public IActionResult Index()
+    {
+        // Fetch data or from session/localStorage equivalent on server-side
+        var resultData = new
+        {
+            LotNo = "L001",
+            GemID = "ID123",
+            Size = "5.2 mm",
+            Remarks = "Sample record",
+            ImageUrl = "/images/placeholder.png"
+        };
+
+        return View(resultData); // Pass data to the Result.cshtml view
+    }
+}
